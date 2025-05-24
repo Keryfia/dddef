07b67a13a4004c80d880ee8422a04176d22d0759
Keryfia <keryfia@gmail.com>
2025-05-24 14:43:36 +0200

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -3,10 +3,10 @@ import json
 import re
 import os
 
-USREPG = ""
-BRANCHEPG = ""
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+USREPG = "Keryfia"
+BRANCHEPG = "dddef"
+MFPLINK = "https://keryfia-viennari.hf.space"     # non mettere lo / finale al link
+MFPPSW = "123"
 
 
 PROXY = f"{MFPLINK}/proxy/hls/manifest.m3u8?api_password={MFPPSW}&d="
